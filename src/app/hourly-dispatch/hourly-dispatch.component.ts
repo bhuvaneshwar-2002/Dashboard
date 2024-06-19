4eded2830e33c0d8ac91fda9bb75c9d1493128be
Bhuvaneshwar-2002 <bhuvaneshwar2002s@gmail.com>
2024-06-19 15:26:55 +0530

    hrly dispatch edited

--- src/app/hourly-dispatch/hourly-dispatch.component.ts
+++ src/app/hourly-dispatch/hourly-dispatch.component.ts
@@ -1,5 +1,9 @@
 import { Component, ViewChild } from "@angular/core";
 import { NgApexchartsModule } from "ng-apexcharts";
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
+import { Slider, SliderModule } from 'primeng/slider';
+import { InputSwitchModule } from 'primeng/inputswitch';
 import { MatCard, MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import {
@@ -8,29 +12,33 @@ import {
   ApexChart,
   ApexXAxis,
   ApexDataLabels,
-  ApexTooltip,
+  ApexTitleSubtitle,
   ApexStroke,
-  ApexFill
+  ApexGrid
 } from "ng-apexcharts";
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
   xaxis: ApexXAxis;
-  stroke: ApexStroke;
-  tooltip: ApexTooltip;
   dataLabels: ApexDataLabels;
-  fill: ApexFill;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
 };
 
 @Component({
   selector: 'app-hourly-dispatch',
   standalone: true,
-  imports: [NgApexchartsModule, MatCard, MatCardModule, MatButtonModule],
-  templateUrl: './hourly-dispatch.component.html',
-  styleUrls: ['./hourly-dispatch.component.css']
+  imports: [ NgApexchartsModule, CalendarModule,InputSwitchModule,SliderModule,
+    FormsModule ,MatCard, MatCardModule, MatButtonModule],
+  templateUrl: "./hourly-dispatch.component.html",
+  styleUrls: ["./hourly-dispatch.component.css"]
 })
 export class HourlyDispatchComponent {
+  date: Date | undefined;
+  checked: boolean = false;
+  value!: number;
   @ViewChild("chart") chart: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
 
@@ -39,85 +47,65 @@ export class HourlyDispatchComponent {
       series: [
         {
           name: "Asset In",
-          data: [31, 40, 28, 51, 42, 109, 100, 45, 78, 56, 12, 45, 46]
-        },
-        {
-          name: "Asset OUT",
-          data: [11, 32, 45, 32, 34, 52, 41, 97, 46, 13, 89, 56, 28, 89]
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148,100,52,89,10, 41, 35, 51, 49, 62, 69, 91, 148,100,52,89],
+          color:'#ff7b7b',
         }
       ],
       chart: {
-        height: 300,
-        width: 600,
-        type: "area"
+        height: 290,
+        type: "line",
+        zoom: {
+          enabled: false
+        }
       },
       dataLabels: {
         enabled: false
       },
       stroke: {
-        curve: "smooth",
-        colors: ['#FF5733', '#33FF57'] // Change the curve colors here
+        curve: "straight",
+        width:2,
       },
-      fill: {
-        type: "gradient",
-        gradient: {
-          shadeIntensity: 1,
-          opacityFrom: 0.7,
-          opacityTo: 0.9,
-          stops: [0, 90, 100],
-          colorStops: [
-            {
-              offset: 0,
-              color: "#FFFFFF",
-              opacity: 0.7
-            },
-            {
-              offset: 100,
-              color: "#FFFFFF",
-              opacity: 0.9
-            }
-          ]
+      title: {
+        text: "Assets by Month",
+        align: "left",
+
+      },
+
+      grid: {
+        row: {
+          colors: ["#ffbaba", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.1
         }
       },
       xaxis: {
-        type: "datetime",
         categories: [
-          "2018-09-19T00:00:00.000Z",
-          "2018-09-19T01:30:00.000Z",
-          "2018-09-19T02:30:00.000Z",
-          "2018-09-19T03:30:00.000Z",
-          "2018-09-19T04:30:00.000Z",
-          "2018-09-19T05:30:00.000Z",
-          "2018-09-19T06:30:00.000Z",
-          "2018-09-19T07:00:00.000Z",
-          "2018-09-19T08:30:00.000Z",
-          "2018-09-19T09:30:00.000Z",
-          "2018-09-19T10:30:00.000Z",
-          "2018-09-19T11:30:00.000Z",
-          "2018-09-19T12:30:00.000Z",
-          "2018-09-19T13:30:00.000Z",
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "20",
+          "21",
+          "22",
+          "23",
+          "24",
+
         ]
-      },
-      tooltip: {
-        x: {
-          format: "dd/MM/yy HH:mm"
-        }
       }
     };
-  }
-
-  public generateData(baseval: number, count: number, yrange: { max: number; min: number; }) {
-    var i = 0;
-    var series = [];
-    while (i < count) {
-      var x = Math.floor(Math.random() * (750 - 1 + 1)) + 1;
-      var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-      var z = Math.floor(Math.random() * (75 - 15 + 1)) + 15;
-
-      series.push([x, y, z]);
-      baseval += 86400000;
-      i++;
-    }
-    return series;
   }
 }
